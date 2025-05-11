6d5eef32a69f4f266f6b7560694ad68f617e7e5d
Syntax Surge <syntaxsurge@gmail.com>
2025-05-12 02:00:54 +0800

    Remove smoldot worker

--- providers/worker.ts
+++
@@ -1,5 +0,0 @@
-import { compileBytecode } from 'smoldot/bytecode'
-import * as smoldot from 'smoldot/worker'
-
-compileBytecode().then((bytecode) => postMessage(bytecode))
-onmessage = (msg) => smoldot.run(msg.data)
